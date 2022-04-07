e0eb3a8b9b5dc46089d0ae6bc55a603d3fe6fb31
VeniVidiGiti <99083579+VENIVIDIGITl@users.noreply.github.com>
2022-04-07 20:47:41 +0300

    add volume icon mute/unmute toggle functionality

--- script.js
+++ script.js
@@ -57,6 +57,8 @@ function setProgress(event) {
 
 // Volume Controls
 
+let lastVolume = 1;
+
 // Volume Bar
 function changeVolume(event) {
   let volume = event.offsetX / volumeRange.offsetWidth;
@@ -78,6 +80,24 @@ function changeVolume(event) {
   } else if (volume === 0) {
     volumeIcon.classList.add('fas', 'fa-volume-off');
   }
+  lastVolume = volume;
+}
+
+// Mute / Unmute
+function toggleMute() {
+  volumeIcon.className = '';
+  if (video.volume) {
+    lastVolume = video.volume;
+    video.volume = 0;
+    volumeBar.style.width = 0;
+    volumeIcon.classList.add('fas', 'fa-volume-mute');
+    volumeIcon.setAttribute('title', 'Unmute');
+  } else {
+    video.volume = lastVolume;
+    volumeBar.style.width = `${lastVolume * 100}%`;
+    volumeIcon.classList.add('fas', 'fa-volume-up');
+    volumeIcon.setAttribute('title', 'Mute');
+  }
 }
 
 // Change Playback Speed
@@ -92,3 +112,4 @@ video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
 volumeRange.addEventListener('click', changeVolume);
+volumeIcon.addEventListener('click', toggleMute);
